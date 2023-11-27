21a39500e9cc7547ab9e9f97eb95dbf8a8736978
ahmed <elshehry@gamil.com>
2023-11-27 15:19:15 +0200

    Integrate chat header

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import axios from "axios";
+import { redirect } from "next/navigation";
+import { redirectToSignIn } from "@clerk/nextjs";
 
-export default function ServerPage() {
-  return <div>ServerPage</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { IChannel, IProfile, IServer } from "@/types/data-types";
+
+interface ServerPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+export default async function ServerIdPage({ params }: ServerPageProps) {
+  const profile: IProfile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const { data } = await axios.get(
+    `${process.env.API_URL}api/v1/servers/${params.serverId}`
+  );
+
+  const server: IServer = data.data.data;
+
+  const initialChannel: IChannel = server.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel._id}`);
 }
